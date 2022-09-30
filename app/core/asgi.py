72ec8862a86e82734436b835c7bbbea348ea5064
Lautarotetamusa <lautarotetamusa@gmail.com>
2022-09-30 11:29:17 -0300

    delete rounting file

--- app/core/asgi.py
+++ app/core/asgi.py
@@ -5,12 +5,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from django.urls import re_path
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-from home.routing import websocket_urlpatterns
+from home.consumers import CreateBotConsumer
+
+websocket_urlpatterns = [
+    re_path(r'new_bot/$', CreateBotConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
